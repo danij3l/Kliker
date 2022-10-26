7074ea93d973e0d2c84dd6b42037fd5958a54c73
Danijel <danijelpavic21@gmail.com>
2022-10-27 00:23:50 +0200

    Decoration.

--- main.py
+++ main.py
@@ -38,36 +38,6 @@ from bs4 import BeautifulSoup
 #                                                                            category -> KATEGORIJA U KOJOJ JE KNJIGA SVRSTANA (New and recent, Bestselling History Titles, ...)
 
 
-
-class ClassTemplate():
-    def __init__(self, name="ClassTemplate"):
-        self.results = []
-    
-    def parse(self, soup):
-        print("CODE HERE")
-        
-        #   PRIMJER LISTE OBJEKATA (ATRIBUTI SU DUMMY PODATCI, SLUŽI SAMO KAO PRIMJER STRUKTURE OUTPUTA)
-        self.results = [
-            {
-                "title": "Object 1",
-                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas bibendum nisl eu maximus convallis. Maecenas suscipit, augue eu viverra suscipit, quam est luctus neque, vel consequat eros felis eu eros. Ut mollis suscipit vulputate. Aliquam erat volutpat. Phasellus nec dui aliquet, ornare urna vitae, placerat justo. Fusce mauris ipsum, porttitor sed cursus id, venenatis a erat. Nulla dui urna, semper sit amet porta in, dapibus eget ante. Sed consequat lacinia vestibulum. Donec porttitor nisl vel dui congue vulputate. Quisque hendrerit neque vel libero lobortis efficitur.",
-                "price": 100
-            },
-            {
-                "title": "Object 2",
-                "description": "Proin ac tortor et nisl pretium hendrerit eu sit amet arcu. Integer risus purus, varius eget dui auctor, dictum mollis lacus. Etiam sit amet metus ante. Donec et tortor mattis, efficitur purus vitae, porta arcu. Phasellus dictum, felis viverra euismod tristique, libero lorem facilisis odio, non luctus diam nisl vitae turpis. Aliquam at turpis quis nisl placerat tincidunt. Nam a hendrerit elit. Vivamus eget mi porta, pulvinar massa in, eleifend augue. ",
-                "price": 150
-            },
-            {
-                 "title": "Object 2",
-                 "description": "Integer neque ex, tincidunt in suscipit in, elementum sit amet neque. Quisque pulvinar dui eget tellus porttitor, sed fringilla ligula imperdiet. Sed eu sem justo. Etiam a ullamcorper ipsum. Pellentesque varius orci vel velit cursus suscipit. Proin at congue nisl. Suspendisse vitae dolor in risus molestie tempor. Suspendisse nec metus magna. Proin sed quam lectus. Proin scelerisque dui purus, aliquam vehicula purus commodo id. Praesent fermentum ante semper erat rhoncus, eu congue ligula sollicitudin. Pellentesque viverra vehicula auctor. Sed quis lacus sem. ",
-                 "price": 250
-            }
-        ]
-        
-        return self.results
-
-
 class QuotesToScrapeClass():
     def __init__(self, name="QuotesToScrapeClass"):
         self.results = []
@@ -99,14 +69,6 @@ class QuotesToScrapeClass():
 class BooksToScrapeClass():
     def __init__(self, name="BooksToScrapeClass"):
         self.results = []
-
-#   https://books.toscrape.com/
-#   books_to_scrape_test.html i MORAJA VRATITI LISTU OBJEKTA KOJI SVI IMAJU:
-#                                                                            url -> URL NA KOJEM SE MOGU VIDJETI DETALJI TE KNJIGE
-#                                                                            title -> TITLE TE KNJIGE (PO MOGUĆNOSTI UZETI CIJELI TITLE)
-#                                                                            stock -> 0 ILI 1 OVISNO O JELI KNJIGA DOSTUPNA ZA KUPITI (0-NE, 1-DA)
-#                                                                            price -> CIJENA KJNIGE (MORA BITI float TIP)
-#                                                                            rating -> RATING KNJIGE (OD 0-5, OVISNO KOLIKO ZVIJEZDA IMA KNJIGA)
     
     def parse(self, soup):
 	
@@ -151,12 +113,14 @@ class BooksToScrapeClass():
 
         return booksObjectList
 
+
 class BookRepositiryToScrapeClass():
     def __init__(self, name="BookRepositiryToScrapeClass"):
         self.results = []
     
-    def parse(self, soup):     
-        return "Not yet implemented ^^"
+    def parse(self, soup):  
+        bookRepositiryObjectList = [] 
+        return
 
 
 if __name__ == '__main__':
