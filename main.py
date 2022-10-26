eb82540a16e1167f2a66fd4ac9b074ba6286fc12
Danijel <danijelpavic21@gmail.com>
2022-10-27 00:01:07 +0200

    Impemented BooksToScrapeClass.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cgitb import text
+from operator import contains
 import sys
 import re
 from bs4 import BeautifulSoup
@@ -98,9 +99,57 @@ class QuotesToScrapeClass():
 class BooksToScrapeClass():
     def __init__(self, name="BooksToScrapeClass"):
         self.results = []
+
+#   https://books.toscrape.com/
+#   books_to_scrape_test.html i MORAJA VRATITI LISTU OBJEKTA KOJI SVI IMAJU:
+#                                                                            url -> URL NA KOJEM SE MOGU VIDJETI DETALJI TE KNJIGE
+#                                                                            title -> TITLE TE KNJIGE (PO MOGUĆNOSTI UZETI CIJELI TITLE)
+#                                                                            stock -> 0 ILI 1 OVISNO O JELI KNJIGA DOSTUPNA ZA KUPITI (0-NE, 1-DA)
+#                                                                            price -> CIJENA KJNIGE (MORA BITI float TIP)
+#                                                                            rating -> RATING KNJIGE (OD 0-5, OVISNO KOLIKO ZVIJEZDA IMA KNJIGA)
     
-    def parse(self, soup):     
-        return "Not yet implemented ^^"
+    def parse(self, soup):
+	
+        booksObjectList = []
+
+        for book in soup.find_all("h3"): # interator
+
+            books = soup.find_all("h3")
+            for book in books:
+                url = book.find("a").get('href')
+            
+            products = soup.find_all(class_ = "image_container")
+            for product in products:
+                title = product.select('img')[0]['alt']
+
+            productData = soup.find_all(class_ = "instock availability")
+
+            for pd in productData:
+                x = pd.text
+
+                if "In stock" in x:
+                    stock = 1
+                else: stock=0
+
+            prices = soup.find_all(class_ = "price_color")
+            for price in prices:
+                priceFloatedAndWithoutPoundPrefix = float(price.text[1:]) # [1:] erases pound symbol from price
+
+            ratings = soup.find_all(class_ = "product_pod")
+            for rating in ratings:
+                r = rating.p['class'][1] # fetches second word in rating
+
+                numericalRating=0 #default
+
+                if r == "One": numericalRating = 1
+                elif r == "Two":numericalRating = 2
+                elif r == "Three":numericalRating = 3
+                elif r == "Four":numericalRating = 4
+                elif r == "Four":numericalRating = 5
+
+            booksObjectList.append( (url, title, stock, priceFloatedAndWithoutPoundPrefix, numericalRating) )
+
+        return booksObjectList
 
 class BookRepositiryToScrapeClass():
     def __init__(self, name="BookRepositiryToScrapeClass"):
